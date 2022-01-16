44f307e4893d2588ca1a3aaf8ca97693ceba09bc
Cengizhan <97846543+UchihaCengiz34@users.noreply.github.com>
2022-01-17 00:27:12 +0300

    Update birinciders.py

--- birinciders.py
+++ birinciders.py
@@ -1,2 +1,9 @@
 #32424 yazıldı
 print("32424")
+#şimdi iş yerinde kalma günleri gidiş dönüs mafraflarını yazacağız
+print(22*(1.5+1.4))
+#şimdi yazdığımız sayının rakamlarını söyleyecek bize
+len("ewfwe")
+#dict veri tipinin kullanımı
+kisi = dict(ad="cengiz",soyad="yılsız")
+print(kisi)
